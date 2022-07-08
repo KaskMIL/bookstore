eb054d27d25cbd556dd8dd1e59de9ece9b8ea49a
toMilanesi <tomi.milanesi@gmail.com>
2022-07-08 17:10:12 -0300

    Added BookList component

---
+++ src/components/BookList/BookList.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+// Components
+import Book from '../Book/Book';
+
+const BookList = () => {
+  return (
+    <ul>
+      <Book />
+    </ul>
+  )
+}
+
+export default BookList;
