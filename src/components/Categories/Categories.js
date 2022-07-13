f9fcbf4c4cc3f90490822fc5ff71fa2e0900a1ee
toMilanesi <tomi.milanesi@gmail.com>
2022-07-13 00:56:04 -0300

    Added useDispatch and functionality to categories button

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { checkStatus } from '../../redux/Categories/CategoriesRedux';
 import Header from '../Header/Header';
 
 function Categories() {
+  const dispatch = useDispatch();
+
+  const checkCategories = (e) => {
+    e.preventDefault();
+    dispatch(checkStatus());
+  };
   return (
     <>
       <Header />
-      <button type="submit">Check Status</button>
+      <button onClick={(e) => checkCategories(e)} type="submit">
+        Check Status
+      </button>
     </>
   );
 }
