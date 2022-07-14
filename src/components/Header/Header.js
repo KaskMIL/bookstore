eaaef6de09f8587e246c5f786a52b63d49c2c9ca
toMilanesi <tomi.milanesi@gmail.com>
2022-07-14 16:05:05 -0300

    Add styles to Header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,16 +1,25 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { ImUser } from 'react-icons/im';
+import styles from './Header.module.scss';
 
 function Header() {
   return (
-    <header>
-      <h1>Bookstore CMS</h1>
+    <header className={styles.header}>
       <nav>
+        <h1>Bookstore CMS</h1>
         <ul>
-          <li><Link to="/">Books</Link></li>
-          <li><Link to="/categories">Categories</Link></li>
+          <li>
+            <Link to="/">Books</Link>
+          </li>
+          <li>
+            <Link to="/categories">Categories</Link>
+          </li>
         </ul>
       </nav>
+      <div className="oval">
+        <ImUser />
+      </div>
     </header>
   );
 }
